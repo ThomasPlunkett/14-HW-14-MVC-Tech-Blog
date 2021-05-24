4ef0e96bd4dda12222b7ecdc044c31aec9518bf9
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-24 17:21:28 -0400

    errors to resolve trying to start server - will return later for testing and to debug. Or request guidance from learning assistant team

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// commenting out below - this threw an error in initial npm start
+// const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -12,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
