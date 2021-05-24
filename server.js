ded9d1fbaef3ec5676a026fb8b26c52c839efd33
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-24 19:22:39 -0400

    modified server.js to comment back in lines 8, 41, 43 back in ... app crashed as a result

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 // const routes = require('./controllers');
 // const helpers = require('./utils/helpers');
 
-// const sequelize = require('./config/connection');
+const sequelize = require('./config/connection');
 
 // Create a new sequelize store using the express-session package
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -38,6 +38,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-// });
+});
