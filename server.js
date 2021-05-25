3ce0186e45911fdecde9db856fcd169b552cbc72
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-24 22:05:28 -0400

    contuning to try and get server working. failing to resolve errors. Setup folder for models with files required

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const express = require('express');
-// const session = require('express-session');
-const exphbs = require('express-handlebars');
-// const routes = require('./controllers');
+const session = require('express-session');
+// const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 // const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connections.js');
 
 // Create a new sequelize store using the express-session package
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
